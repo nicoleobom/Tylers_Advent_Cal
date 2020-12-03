8f3ad4934cff1e65f901d132b6266c877edf4f3e
Nicole O’Bomsawin <nicoleobomsawin@gmail.com>
2020-12-03 14:19:36 -0500

    flips

---
+++ script.js
@@ -0,0 +1,13 @@
+function lockedDate() {
+    date = new Date();
+    day = date.getDate();
+    console.log(day);
+    
+    cards = document.getElementsByClassName('nums');
+
+    if (cards !== day) {
+        cards
+    }
+}
+
+lockedDate();
