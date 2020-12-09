1e40e41d7966a32d7ae47de2a84fbbd8425fcadf
Nicole O’Bomsawin <nicoleobomsawin@gmail.com>
2020-12-09 11:30:37 -0500

    adjusting css for locked cells

--- script.js
+++ script.js
@@ -12,7 +12,9 @@ function lockedDate() {
         if (id.id <= day) {
             id.childNodes[1].childNodes[1].innerText = id.id
         } else {
-            id.childNodes[1].childNodes[1].innerHTML = "<i class='fas fa-lock'></i>"
+            id.childNodes[1].childNodes[1].innerHTML = "<i class='fas fa-lock'></i>";
+            id.childNodes[1].childNodes[1].style.background = "lightgrey";
+            id.childNodes[1].childNodes[1].style.color = "darkgrey"
             id.style.pointerEvents = "none";
         }
     }
